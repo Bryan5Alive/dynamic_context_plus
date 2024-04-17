e45fe72f8b90c5850d2d973686a7fb6196061f44
Bryan5Alive <bryan5alive@gmail.com>
2024-04-16 21:18:17 -0400

    Enhancing dynamic_context
    - Added location, temperature, weather, time_of_day, weekday
    - Added auto_append to automatically inject into the context

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# dynamic_context
+# dynamic_context_plus
 
-Extension for oobabooga's [Text Generation WebUI](https://github.com/oobabooga/text-generation-webui) that replaces the tags {{time}},  {{date}} and {{weekday}} in the character's context with the current time, and date, respectively.
-Additionally it also adds the current time as extra information at the end of the prompt (and optionally the date too).
+## Summary
+An extension for oobabooga's [Text Generation WebUI](https://github.com/oobabooga/text-generation-webui) which gives conversations more context.
 
-This is a simple extension, and this additional context doesn't get saved in the chat history, as it's purpose is only to bias the AI response while keeping in mind the current time, or date.
+It's based on the excellent [dynamic_context](https://github.com/elPatrixF/dynamic_context) extension by elPatrixF.
+
+### Usage option 1
+Set the "auto_append" in config to "true" and it will automatically inject a dynamic sentence into the context.
+
+Just modify **settings.yaml** with the config `dynamic_context-auto_append: true`.
+
+You won't have to add anything to the character files if you use this option.
+
+### Usage option 2
+Add any of the following template strings to your character file and they will be replaced.
+- {{location}} with the given location
+- {{temperature}} with the selected temperature
+- {{weather}} with the selected weather
+- {{time}} with the current time
+- {{time_of_day}} with "morning", "afternoon", "evening" or "night"
+- {{date}} with the current data
+- {{season}} with the "spring", "summer", "autumn" or "winter" (based on wester hemisphere)
+- {{weekday}} with the day of the week
+
+### Additional notes
+It can also append the current time or date as extra information at the end of the prompt.
--- script.py
+++ script.py
@@ -1,39 +1,97 @@
-import os
-import gradio as gr
 from datetime import datetime
 
+import gradio as gr
+
 params = {
-	'timecontext': True,
-	'datecontext': False,
+    'location': '',
+    'temperature': 'pleasant',
+    'weather': 'clear',
+    'time_context': False,
+    'date_context': False,
+    'auto_append': False
 }
 
-def	custom_generate_chat_prompt(user_input, state, **kwargs):
-	'''
-	This extension will replace all instances of
-	{{time}} and {{date}} with the current
-	time and date, respectively
-	Additionall replaces {{weekday}} with the day of the week.
-	'''
-	state['context'] = state['context'].replace('{{time}}',datetime.now().strftime("%I:%M %p")) 
-	state['context'] = state['context'].replace('{{date}}',datetime.now().strftime("%B %d, %Y"))
-	state['context'] = state['context'].replace('{{weekday}}',("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")[datetime.now().weekday()])
-	# print(f'<<context, changed>>')
-	# print(f'{state["context"]}')
-	return 
+
+def state_modifier(state):
+    month = datetime.now().month
+    day = datetime.now().day
+
+    if (month > 3 and month < 6) or (month == 3 and day >= 20) or (month == 6 and day < 21):
+        season = 'spring'
+    elif (month > 6 and month < 9) or (month == 6 and day >= 21) or (month == 9 and day < 22):
+        season = 'summer'
+    elif (month > 9 and month < 12) or (month == 9 and day >= 22) or (month == 12 and day < 21):
+        season = 'autumn'
+    else:
+        season = 'winter'
+
+    hour = datetime.now().hour
+    if 5 <= hour < 12:
+        time_of_day = 'morning'
+    elif 12 <= hour < 17:
+        time_of_day = 'afternoon'
+    elif 17 <= hour < 21:
+        time_of_day = 'evening'
+    else:
+        time_of_day = 'night'
+
+    weekday = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')[datetime.now().weekday()]
+
+    if not params['auto_append']:
+        state['context'] = state['context'].replace('{{location}}', params['location'])
+        state['context'] = state['context'].replace('{{temperature}}', params['temperature'])
+        state['context'] = state['context'].replace('{{weather}}', params['weather'])
+        state['context'] = state['context'].replace('{{time}}', datetime.now().strftime('%I:%M %p'))
+        state['context'] = state['context'].replace('{{time_of_day}}', time_of_day)
+        state['context'] = state['context'].replace('{{date}}', datetime.now().strftime('%B %d, %Y'))
+        state['context'] = state['context'].replace('{{season}}', season)
+        state['context'] = state['context'].replace('{{weekday}}', weekday)
+    else:
+        auto_context = get_auto_context(season, time_of_day, weekday)
+        if ('context' in state) and state['context']:
+            state['context'] += '\n\n' + auto_context + '\n'
+        else:
+            state['context'] = auto_context + '\n'
+
+    return state
+
+
+def get_auto_context(season, time_of_day, weekday):
+    base_template = f'This conversation takes place on a {weekday} {time_of_day}'
+
+    if ('location' in params) and params['location']:
+        base_template += f', in {params["location"]}'
+
+    if season:
+        base_template += f', during the {season} season'
+
+    if (('temperature' in params) and params['temperature']) and (('weather' in params) and params['weather']):
+        base_template += f', with a {params["temperature"]} temperature and {params["weather"]} weather'
+
+    return base_template + '.'
 
 
 def input_modifier(string):
-	"""
-	This function adds time or date context to each prompt.
-	"""
-	if params['timecontext'] : string += f' [Current time: {datetime.now().strftime("%I:%M %p")}]'
-	if params['datecontext'] : string += f' [Current date: {datetime.now().strftime("%B %d, %Y")}]'
-	return string
+    if params['time_context']:
+        string += f' [Current time: {datetime.now().strftime("%I:%M %p")}]'
+    if params['date_context']:
+        string += f' [Current date: {datetime.now().strftime("%B %d, %Y")}]'
+    return string
 
 
 def ui():
-	with gr.Accordion("Prompt context"):
-		timecontext = gr.Checkbox(value=params['timecontext'], label='Add current time context to the end of the prompt')
-		datecontext = gr.Checkbox(value=params['datecontext'], label='Add current date context to the end of the prompt')
-	timecontext.change(lambda x: params.update({"timecontext": x}), timecontext, None)
-	datecontext.change(lambda x: params.update({"datecontext": x}), datecontext, None)
+    with gr.Accordion('Dynamic context'):
+        with gr.Row():
+            with gr.Column():
+                location = gr.Textbox(placeholder=params['location'], value=params['location'], label='Location')
+                temperature = gr.Dropdown(['hot', 'warm', 'pleasant', 'chilly', 'cold'], value=params['temperature'], allow_custom_value=True, label='Temperature', type='value')
+                weather = gr.Dropdown(['clear', 'rainy', 'snowy', 'foggy'], value=params['weather'], allow_custom_value=True, label='Weather', type='value')
+            with gr.Column():
+                time_context = gr.Checkbox(value=params['time_context'], label='Append current time context to the prompt')
+                date_context = gr.Checkbox(value=params['date_context'], label='Append current date context to the prompt')
+
+    location.change(lambda x: params.update({'location': x}), location, None)
+    temperature.change(lambda x: params.update({'temperature': x}), temperature, None)
+    weather.change(lambda x: params.update({'weather': x}), weather, None)
+    time_context.change(lambda x: params.update({'time_context': x}), time_context, None)
+    date_context.change(lambda x: params.update({'date_context': x}), date_context, None)
